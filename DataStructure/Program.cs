e657a0ea79835b547a9aeb4e0174559c07cac450
Renuka <renu.bagave1901@gmail.com>
2024-04-26 17:15:36 +0530

    [Renuka]Add : Created Project for data structure examples

---
+++ DataStructure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("data structure assignment");
+        }
+        
+    }
+}
