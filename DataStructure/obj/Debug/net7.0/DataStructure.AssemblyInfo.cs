0a4ccd45b59bd8a9f06d5852768c07ede2c608b2
Renuka <renu.bagave1901@gmail.com>
2024-04-27 10:44:25 +0530

    [Renuka]Refactor : Made changes in syntax of program

--- DataStructure/obj/Debug/net7.0/DataStructure.AssemblyInfo.cs
+++ DataStructure/obj/Debug/net7.0/DataStructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e10d9226623e6f647eae38c88b87983d8ffdb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee52751e644b56e6e292f3b401e993ebd5d91e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
